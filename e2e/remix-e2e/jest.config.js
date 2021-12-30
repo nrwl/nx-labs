39ef0bf61eb43fb004b611a4796c428e525af633
Jack Hsu <jack.hsu@gmail.com>
2021-12-29 23:05:40 -0500

    chore(repo): rename nx-remix to remix (#2)

--- e2e/remix-e2e/jest.config.js
+++ e2e/remix-e2e/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'nx-remix-e2e',
+  displayName: 'remix-e2e',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/e2e/nx-remix-e2e',
+  coverageDirectory: '../../coverage/e2e/remix-e2e',
 };
--- packages/remix/jest.config.js
+++ packages/remix/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'nx-remix',
+  displayName: 'remix',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/nx-remix',
+  coverageDirectory: '../../coverage/packages/remix',
 };
