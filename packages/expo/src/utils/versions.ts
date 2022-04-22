efb2089e32d65985781d482581872e278cd9cf18
Emily Xiong <xiongemi@gmail.com>
2022-04-22 14:57:35 -0400

    feature(expo): add warning for react 18 for expo (#37)

--- packages/expo/src/utils/versions.ts
+++ packages/expo/src/utils/versions.ts
@@ -10,6 +10,11 @@ export const expoUpdatesVersion = '0.11.6';
 export const jestExpoVersion = '44.0.1';
 export const expoCliVersion = '5.3.0';
 
+// TODO(Emily): Remove this once expo supports react-native 0.68.0.
+// v18 is only supported on the New Architecture
+// See: https://reactnative.dev/blog/2022/03/15/an-update-on-the-new-architecture-rollout
+export const reactVersion = '17.0.1';
+
 export const reactNativeVersion = '0.64.3';
 export const typesReactNativeVersion = '0.64.3';
 export const reactNativeWebVersion = '0.17.5';
@@ -20,7 +25,7 @@ export const reactNativeScreensVersion = '3.10.2';
 
 export const reactNativeSvgTransformerVersion = '1.0.0';
 export const reactNativeSvgVersion = '12.1.1';
-export const svgrWebpackVersion = '^5.5.0'
+export const svgrWebpackVersion = '^5.5.0';
 
 export const metroVersion = '0.66.2';
 
