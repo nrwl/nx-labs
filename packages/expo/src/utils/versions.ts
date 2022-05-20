21ae9f14490e36a4937c94d7e0cb18064d9c3370
Emily Xiong <xiongemi@gmail.com>
2022-05-20 18:02:28 -0400

    feat(expo): upgrade expo 45.0.4

--- packages/expo/src/utils/versions.ts
+++ packages/expo/src/utils/versions.ts
@@ -1,34 +1,29 @@
 export const nxVersion = '*';
 
-export const expoVersion = '44.0.6';
-export const expoDevClientVersion = '0.8.5';
-export const expoStatusBarVersion = '1.2.0';
-export const expoMetroConfigVersion = '0.3.16';
+export const expoVersion = '45.0.4';
+export const expoDevClientVersion = '~0.9.6';
+export const expoStatusBarVersion = '~1.3.0';
+export const expoMetroConfigVersion = '0.3.17';
 export const expoStructuredHeadersVersion = '2.1.1';
-export const expoSplashScreenVersion = '0.14.2';
-export const expoUpdatesVersion = '~0.11.7';
-export const jestExpoVersion = '44.0.1';
-export const expoCliVersion = '5.4.3';
-export const babelPresetExpoVersion = '~9.0.2';
+export const expoSplashScreenVersion = '0.15.1';
+export const expoUpdatesVersion = '~0.13.1';
+export const jestExpoVersion = '45.0.1';
+export const expoCliVersion = '5.4.6';
+export const babelPresetExpoVersion = '~9.1.0';
 
-// TODO(Emily): Remove this once expo supports react-native 0.68.0.
-// v18 is only supported on the New Architecture
-// See: https://reactnative.dev/blog/2022/03/15/an-update-on-the-new-architecture-rollout
-export const reactVersion = '17.0.1';
-
-export const reactNativeVersion = '0.64.3';
-export const typesReactNativeVersion = '0.64.3';
-export const reactNativeWebVersion = '0.17.5';
-export const reactNativeGestureHandlerVersion = '2.1.0';
-export const reactNativeReanimatedVersion = '2.3.1';
-export const reactNativeSafeAreaContextVersion = '3.3.2';
-export const reactNativeScreensVersion = '3.10.2';
+export const reactNativeVersion = '0.68.2';
+export const typesReactNativeVersion = '0.67.7';
+export const reactNativeWebVersion = '0.17.7';
+export const reactNativeGestureHandlerVersion = '~2.2.1';
+export const reactNativeReanimatedVersion = '~2.8.0';
+export const reactNativeSafeAreaContextVersion = '4.2.4';
+export const reactNativeScreensVersion = '~3.11.1';
 
 export const reactNativeSvgTransformerVersion = '1.0.0';
-export const reactNativeSvgVersion = '12.1.1';
+export const reactNativeSvgVersion = '12.3.0';
 export const svgrWebpackVersion = '^5.5.0';
 
-export const metroVersion = '0.70.2';
+export const metroVersion = '0.70.3';
 
-export const testingLibraryReactNativeVersion = '9.0.0';
-export const testingLibraryJestNativeVersion = '4.0.4';
+export const testingLibraryReactNativeVersion = '9.1.0';
+export const testingLibraryJestNativeVersion = '4.0.5';
