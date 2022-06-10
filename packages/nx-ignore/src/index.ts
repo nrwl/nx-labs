f397e7c94af57d8d51ab3bec1828fffb9f309814
Jack Hsu <jack.hsu@gmail.com>
2022-06-10 09:30:40 -0400

    feat(nx-ignore): add --verbose option for debugging

--- packages/nx-ignore/src/index.ts
+++ packages/nx-ignore/src/index.ts
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 import { findWorkspaceRoot } from 'nx/src/utils/find-workspace-root';
 
-const project = process.argv[2];
+const args = process.argv.slice(2);
+const project = args.find((s) => !s.startsWith('-')) as string;
+const isVerbose = args.some((s) => s === '--verbose');
+const baseSha = 'HEAD^';
+const headSha = 'HEAD';
 
 if (!project) {
   console.log('≫ No project passed to nx-ignore script');
+  process.exit(1);
 }
 
 console.log(
@@ -23,17 +28,25 @@ main();
 async function main() {
   // Since Nx currently looks for "nx" package under workspace root, the CLI doesn't work on Vercel.
   const { affected } = await import('nx/src/command-line/affected');
-  let result = null;
+  let result = { projects: [] as string[] };
+
+  logDebug(`\n≫ Comparing ${baseSha}...${headSha}\n`);
+
+  // TODO(jack): This console.log hack isn't needed if Nx can support being installed outside of cwd.
   const _log = console.log;
+
   try {
     let output = '';
     console.log = (x) => {
       output = x;
     };
+
     await affected('print-affected', {
       _: '',
-      base: 'HEAD^',
+      base: baseSha,
+      head: headSha,
     } as any);
+
     result = JSON.parse(output);
   } catch (e) {
     console.error(e);
@@ -42,10 +55,18 @@ async function main() {
     console.log = _log;
   }
 
+  logDebug(`≫ Affected projects:\n  - ${result.projects.join('\n  - ')}\n`);
+
   if (result.projects.includes(project)) {
-    console.log(`≫ ✅ - Build can proceed since ${project} is affected`);
+    console.log(`✅ - Build can proceed since ${project} is affected`);
     process.exit(1); // this tells vercel to not ignore
   } else {
-    console.log(`≫ 🛑 - Build cancelled since ${project} is not affected`);
+    console.log(`🛑 - Build cancelled since ${project} is not affected`);
   }
+
+  process.exit(0);
+}
+
+function logDebug(s: string) {
+  if (isVerbose) console.log(s);
 }
