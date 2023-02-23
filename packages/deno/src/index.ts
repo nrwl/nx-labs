b886be74a054287f3406482ebfba660539e5e3ef
Chau Tran <nartc7789@gmail.com>
2023-02-23 15:57:17 -0600

    fix(deno): expose generator (#155)

--- packages/deno/src/index.ts
+++ packages/deno/src/index.ts
@@ -1,1 +1,4 @@
+export { default as applicationGenerator } from './generators/application/generator';
+export { default as initGenerator } from './generators/init/generator';
+export { default as libraryGenerator } from './generators/library/library';
 export { processProjectGraph } from './graph/process-graph';
