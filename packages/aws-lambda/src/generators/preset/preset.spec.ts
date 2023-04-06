6553ad29fa834a5e840008d0158abf22cf767e03
Nicholas Cunningham <ndcunningham@gmail.com>
2023-04-06 13:07:57 -0600

    fix(aws-lambda) Remove unsupported option which would fail to generate

---
+++ packages/aws-lambda/src/generators/preset/preset.spec.ts
@@ -0,0 +1,20 @@
+import { Tree } from "@nrwl/devkit";
+import { createTreeWithEmptyWorkspace } from "nx/src/devkit-testing-exports";
+import awsLambdaPreset from './preset';
+
+describe('aws-lambda preset', () => {
+    let tree: Tree;
+
+    beforeEach(() =>  {
+        tree = createTreeWithEmptyWorkspace();
+    })
+
+    it('should create a default aws-lambda workspace', async () => {
+        await awsLambdaPreset(tree, {
+            name: 'aws-demo'
+        });
+
+        expect(tree.exists('samconfig.toml'));
+        expect(tree.exists('template.yaml'));
+    });
+});
