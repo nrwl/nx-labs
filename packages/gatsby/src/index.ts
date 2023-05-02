b3a7a92f8e1a3b6345e7fad9c846a485b4178aee
Caleb Ukle <caleb.ukle+github@pm.me>
2023-05-02 13:12:03 -0500

    chore(gatsby): update legacy

---
+++ packages/gatsby/src/index.ts
@@ -0,0 +1,4 @@
+export { applicationGenerator } from './generators/application/application';
+export { componentGenerator } from './generators/component/component';
+export { gatsbyInitGenerator } from './generators/init/init';
+export { pageGenerator } from './generators/page/page';
