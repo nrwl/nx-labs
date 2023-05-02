37b50147518d445d88e70db150e3fe8e56becd29
Caleb Ukle <caleb.ukle+github@pm.me>
2023-05-02 11:45:42 -0500

    feat(remix): export generators

---
+++ packages/remix/src/index.ts
@@ -0,0 +1,23 @@
+import actionGenerator from './generators/action/action.impl';
+import applicationGenerator from './generators/application/application.impl';
+import cypressGenerator from './generators/cypress/cypress.impl';
+import libraryGenerator from './generators/library/library.impl';
+import loaderGenerator from './generators/loader/loader.impl';
+import metaGenerator from './generators/meta/meta.impl';
+import presetGenerator from './generators/preset/preset.impl';
+import resourceRouteGenerator from './generators/resource-route/resource-route.impl';
+import routerGenerator from './generators/route/route.impl';
+import styleGenerator from './generators/style/style.impl';
+
+export {
+  actionGenerator,
+  applicationGenerator,
+  cypressGenerator,
+  libraryGenerator,
+  loaderGenerator,
+  metaGenerator,
+  presetGenerator,
+  resourceRouteGenerator,
+  routerGenerator,
+  styleGenerator,
+};
