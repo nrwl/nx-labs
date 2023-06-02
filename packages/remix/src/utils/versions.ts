8dd103c2dcdf7de91cc7343fc85d858f653e0def
Colum Ferry <cferry09@gmail.com>
2023-06-02 10:01:14 +0100

    feat(remix): add setup-tailwind generator (#277)

--- packages/remix/src/utils/versions.ts
+++ packages/remix/src/utils/versions.ts
@@ -8,10 +8,7 @@ export const typesReactVersion = '^18.0.25';
 export const typesReactDomVersion = '^18.0.8';
 export const eslintVersion = '^8.27.0';
 export const typescriptVersion = '^4.8.4';
-export const testingLibraryReactVersion = '^14.0.0';
-export const testingLibraryJestDomVersion = '^5.16.5';
-export const testingLibraryUserEventsVersion = '^14.4.3';
-
+export const tailwindVersion = '^3.3.0';
 export const testingLibraryReactVersion = '^14.0.0';
 export const testingLibraryJestDomVersion = '^5.16.5';
 export const testingLibraryUserEventsVersion = '^14.4.3';
