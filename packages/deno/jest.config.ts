799d448754cb39a9574775749db4aaa6aa48538b
Stefan Charsley <charsleysa@gmail.com>
2023-06-27 23:21:13 +0000

    fix: undo change to jest.config.ts

--- packages/deno/jest.config.ts
+++ packages/deno/jest.config.ts
@@ -12,5 +12,5 @@ export default {
     ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/deno'
+  coverageDirectory: '../../coverage/packages/deno',
 };
