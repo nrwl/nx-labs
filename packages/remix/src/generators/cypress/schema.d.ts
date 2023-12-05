ed94f399be3f24e9f1770a5fdf1edc13f1fd2fa7
Colum Ferry <cferry09@gmail.com>
2023-12-05 14:29:02 +0000

    feat(remix): projectNameAndRootFormat for e2e generator (#356)

--- packages/remix/src/generators/cypress/schema.d.ts
+++ packages/remix/src/generators/cypress/schema.d.ts
@@ -1,8 +1,11 @@
+import { type ProjectNameAndRootFormat } from '@nx/devkit/src/generators/project-name-and-root-utils';
+
 export interface CypressGeneratorSchema {
   project: string;
   name: string;
   baseUrl?: string;
   directory?: string;
+  projectNameAndRootFormat?: ProjectNameAndRootFormat;
   linter?: 'none' | 'eslint';
   js?: boolean;
   skipFormat?: boolean;
