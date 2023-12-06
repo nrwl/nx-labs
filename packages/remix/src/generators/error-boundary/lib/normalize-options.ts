30329d14a6563e6fb48aee664d857872cd0d973b
Colum Ferry <cferry09@gmail.com>
2023-12-06 17:16:59 +0000

    feat(remix): add consistent path generation for error boundary

--- packages/remix/src/generators/error-boundary/lib/normalize-options.ts
+++ packages/remix/src/generators/error-boundary/lib/normalize-options.ts
@@ -6,15 +6,14 @@ export function normalizeOptions(
   tree: Tree,
   schema: ErrorBoundarySchema
 ): ErrorBoundarySchema {
-  const pathToRouteFile = resolveRemixRouteFile(
-    tree,
-    schema.path,
-    schema.project
-  );
+  const pathToRouteFile =
+    schema.nameAndDirectoryFormat === 'as-provided'
+      ? schema.path
+      : resolveRemixRouteFile(tree, schema.path, schema.project);
 
   if (!tree.exists(pathToRouteFile)) {
     throw new Error(
-      `Route file specified does not exist "${pathToRouteFile}". Please ensure you pass a correct path to the file, relative to the project root.`
+      `Route file specified does not exist "${pathToRouteFile}". Please ensure you pass a correct path to the file.`
     );
   }
 
