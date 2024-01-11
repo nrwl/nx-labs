cedfe024548eba74ed17cac54588dff7a380d08e
Katerina Skroumpelou <sk.katherine@gmail.com>
2024-01-11 16:46:23 +0200

    cleanup(repo): remove old packages from everywhere

--- scripts/nx-release.mjs
+++ scripts/nx-release.mjs
@@ -57,11 +57,7 @@ if (!parsedArgs.local) {
 function updatePackageJsonFiles(parsedVersion, isLocal) {
   let pkgFiles = [
     'package.json',
-    'dist/npm/aws-lambda/package.json',
-    'dist/npm/netlify/package.json',
-    'dist/npm/deno/package.json',
     'dist/npm/nx-ignore/package.json',
-    'dist/npm/remix/package.json',
     'dist/npm/rspack/package.json',
   ];
   if (isLocal) {
