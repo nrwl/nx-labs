5796ac86b149c7e84340cb1fe207d8f89d4d9dc9
Colum Ferry <cferry09@gmail.com>
2024-09-10 20:01:54 +0100

    chore(repo): remove rspack

--- packages/rspack/src/utils/mode-utils.ts
+++
@@ -1,5 +0,0 @@
-import { Mode } from '@rspack/core';
-
-export function isMode(mode: string): mode is Mode {
-  return mode === 'development' || mode === 'production' || mode === 'none';
-}
--- packages/rspack/src/utils/model.ts
+++
@@ -1,7 +0,0 @@
-import { ExecutorContext } from '@nx/devkit';
-import { RspackExecutorSchema } from '../executors/rspack/schema';
-
-export interface SharedConfigContext {
-  options: RspackExecutorSchema;
-  context: ExecutorContext;
-}
