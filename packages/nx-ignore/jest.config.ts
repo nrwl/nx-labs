36f767f06562d17fb8a6dbbdca9e5bb394ee8690
Juri <juri.strumpflohner@gmail.com>
2026-03-18 11:46:08 +0100

    fix(nx-ignore): update jest config to use ts-jest and ES module syntax

--- packages/nx-ignore/jest.config.ts
+++ packages/nx-ignore/jest.config.ts
@@ -1,16 +1,9 @@
-import { readFileSync } from 'fs';
-
-// Reading the SWC compilation config and remove the "exclude"
-// for the test files to be compiled by SWC
-const { exclude: _, ...swcJestConfig } = JSON.parse(
-  readFileSync(`${__dirname}/.lib.swcrc`, 'utf-8')
-);
-
-module.exports = {
+export default {
   displayName: 'nx-ignore',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/nx-ignore',
